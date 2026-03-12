052d9656b7eb29aee6d9480fd43ac52ecab540e1
Davi Francisco <franciscodavi327@gmail.com>
2026-03-12 17:06:06 -0300

    feat: adicionar estrutura básica para rotas com react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
 import './globals.css';
+
+const router = createBrowserRouter([{ index: true, element: <Home /> }]);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 );
