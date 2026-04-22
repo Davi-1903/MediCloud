0cfd87462327270c50a2340ae64d3cd354d41f8c
Maria <garciamariadasgracas318@gmail.com>
2026-04-21 21:31:44 -0300

    fix: corrigir banco de dados

--- server/models/users.py
+++ server/models/users.py
@@ -1,57 +1,55 @@
 from database import Base
 from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 
-class Users(Base):
-    __tablename__ = 'users'
+class User(Base):
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    nome: Mapped[str] = mapped_column(nullable=False)
-    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-    senha: Mapped[str] = mapped_column(String(400), nullable=False)
     tipo: Mapped[str] = mapped_column(String(50), nullable=False)
-
-    rua: Mapped[str] = mapped_column(String(150), nullable=False)
-    bairro: Mapped[str] = mapped_column(String(150), nullable=False)
-    cidade: Mapped[str] = mapped_column(String(150), nullable=False)
-    cep: Mapped[str] = mapped_column(String(9), nullable=False)
-    telefone: Mapped[str] = mapped_column(String(15), nullable=False)
+    nome: Mapped[str] = mapped_column(String(150), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    senha: Mapped[str] = mapped_column(String(255), nullable=False)
+    telefone: Mapped[str] = mapped_column(String(15))
+    rua: Mapped[str] = mapped_column(String(150))
+    bairro: Mapped[str] = mapped_column(String(150))
+    numero: Mapped[int] = mapped_column()
+    cidade: Mapped[str] = mapped_column(String(150))
+    cep: Mapped[str] = mapped_column(String(8))
 
     __mapper_args__ = {
-        'polymorphic_identity': 'user',
-        'polymorphic_on': tipo
+        "polymorphic_identity": "user",
+        "polymorphic_on": tipo
     }
 
-    def get_id(self):
-        return str(self.id)
 
-class Clinicas(Users):
-    __tablename__ = 'clinicas'
+class Medico(User):
+    __tablename__ = "medicos"
 
     id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
-    instagram: Mapped[str] = mapped_column(String(100))
-    facebook: Mapped[str] = mapped_column(String(100))
+    crm: Mapped[str] = mapped_column(String(20), nullable=False)
+    especialidade: Mapped[str] = mapped_column(String(100))
 
     __mapper_args__ = {
-        'polymorphic_identity': 'clinica'
+        "polymorphic_identity": "medico"
     }
 
 
-class Pacientes(Users):
-    __tablename__ = 'pacientes'
-
-    id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'paciente'
-    }
-
-
-class Administradores(Users):
-    __tablename__ = 'administradores'
+class Paciente(User):
+    __tablename__ = "pacientes"
 
     id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
 
     __mapper_args__ = {
-        'polymorphic_identity': 'administrador'
+        "polymorphic_identity": "paciente"
+    }
+
+
+class Administrador(User):
+    __tablename__ = "administradores"
+
+    id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "admin"
     }
