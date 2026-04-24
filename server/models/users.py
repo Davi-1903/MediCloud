2cd84b299b4ec1c2478809edc578af387532cb31
Maria <garciamariadasgracas318@gmail.com>
2026-04-24 16:52:09 -0300

    refactor: tirar a parte de endereço do banco de dados

--- server/models/users.py
+++ server/models/users.py
@@ -4,15 +4,7 @@ class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     email: str = Field(unique=True, index=True)
     senha: str
-    
     nome: str 
-    telefone: str
-    estado: str
-    cidade: str
-    cep: str = Field(max_length=9)
-    rua: str
-    bairro: str
-    numero: int
 
     tipo: str
     
