48f37ea28f39b01991a6ec3cb5edffa97d6deb3d
Maria <garciamariadasgracas318@gmail.com>
2026-05-04 14:23:08 -0300

    feat: iniciar a parte de rota provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react';
+import { lazy, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import './globals.css';
 
-const router = createBrowserRouter([{ index: true, element: <Home /> }]);
+
+const Register = lazy(() => import('./pages/unprotected/register'));
+
+const router = createBrowserRouter([
+    {
+        index: true,
+        element: <Home /> 
+    },
+    {
+        path: 'register',
+        element: <Register />
+    },
+]);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,63 +1,12 @@
-// export default function Home() {
-//     return (
-//         <>
-//             <h1>MediCloud</h1>
-//         </>
-//     );
-// }
+import { Link } from 'react-router-dom';
 
-import { useState } from "react";
-
-export default function Register() {
-    const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-
-        const response = await fetch('http://localhost:8000/auth/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                nome: nome,
-                email: email,
-                senha: senha,
-            }),
-        });
-
-        const data = await response.json();
-        console.log(data);
-    }
-
+export default function Home() {
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='nome'>Nome:</label>
-                <input
-                    type='text'
-                    name='nome'
-                    id='nome'
-                    onChange={e => setNome(e.target.value)}
-                />
-                <label htmlFor='email'>Email:</label>
-                <input
-                    type='email'
-                    name='email'
-                    id='email'
-                    onChange={e => setEmail(e.target.value)}
-                />
-                <label htmlFor='senha'>Senha:</label>
-                <input
-                    type='password'
-                    name='senha'
-                    id='senha'
-                    onChange={e => setSenha(e.target.value)}
-                />
-                <button type='submit'>Enviar</button>
-            </form>
+            <h1>MediCloud</h1>
+            <Link to='/register'>
+                <button>Cadastrar</button>
+            </Link>
         </>
     );
 }
