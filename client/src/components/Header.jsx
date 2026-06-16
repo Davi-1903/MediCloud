6ba6770116c5564faf9ea43fd9fff2040e6866ad
Davi Francisco <franciscodavi327@gmail.com>
2026-06-16 08:08:35 -0300

    style: fazer estilo da página de cadastro

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <header className='fixed top-4 left-4 h-18 w-[calc(100%-2rem)] rounded-2xl border border-[#ED5770] bg-white'></header>
+    );
+}
--- client/src/pages/unprotected/register/index.jsx
+++ client/src/pages/unprotected/register/index.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import Logo from '../../../../public/assets/images/medicloud-logo.png';
+import Header from '../../../components/Header';
 
 export default function Register() {
-    const [nome, setNome] = useState('');
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
+    const [password, setPassword] = useState('');
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -13,11 +15,7 @@ export default function Register() {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                nome: nome,
-                email: email,
-                senha: senha,
-            }),
+            body: JSON.stringify({ name, email, password }),
         });
 
         const data = await response.json();
@@ -25,31 +23,85 @@ export default function Register() {
     }
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='nome'>Nome:</label>
-                <input
-                    type='text'
-                    name='nome'
-                    id='nome'
-                    onChange={e => setNome(e.target.value)}
-                />
-                <label htmlFor='email'>Email:</label>
-                <input
-                    type='email'
-                    name='email'
-                    id='email'
-                    onChange={e => setEmail(e.target.value)}
-                />
-                <label htmlFor='senha'>Senha:</label>
-                <input
-                    type='password'
-                    name='senha'
-                    id='senha'
-                    onChange={e => setSenha(e.target.value)}
-                />
-                <button type='submit'>Enviar</button>
-            </form>
-        </>
+        <div className='h-svh bg-[url(../../../../public/assets/images/background-register.png)] bg-cover bg-center bg-no-repeat'>
+            <Header />
+            <main className='relative h-full'>
+                <form
+                    onSubmit={handleSubmit}
+                    className='absolute top-1/2 left-3/4 flex w-full max-w-lg -translate-1/2 flex-col gap-4 rounded-2xl bg-white p-12 shadow-2xl'
+                >
+                    <img
+                        src={Logo}
+                        alt='Logo'
+                        className='mx-auto h-40 w-7/10 object-cover'
+                    />
+                    <h2 className='text-center text-xl font-semibold'>Crie sua conta</h2>
+                    <span className='text-center text-gray-500'>Preencha os dados abaixo para criar a sua conta</span>
+                    <div>
+                        <label
+                            className='mb-1 block font-medium text-[#D43953]'
+                            htmlFor='nome'
+                        >
+                            Nome completo
+                        </label>
+                        <input
+                            type='text'
+                            name='nome'
+                            id='nome'
+                            placeholder='Digite seu nome completo'
+                            className='h-12 w-full rounded-lg border border-[#D43953] bg-[#FFF5F6] px-4 outline-none'
+                            onChange={e => setName(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label
+                            className='mb-1 block font-medium text-[#D43953]'
+                            htmlFor='email'
+                        >
+                            Email
+                        </label>
+                        <input
+                            type='email'
+                            name='email'
+                            id='email'
+                            placeholder='exemplo@gmail.com'
+                            className='h-12 w-full rounded-lg border border-[#D43953] bg-[#FFF5F6] px-4 outline-none'
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label
+                            className='mb-1 block font-medium text-[#D43953]'
+                            htmlFor='senha'
+                        >
+                            Senha
+                        </label>
+                        <input
+                            type='password'
+                            name='senha'
+                            id='senha'
+                            placeholder='Sua senha secreta...'
+                            className='h-12 w-full rounded-lg border border-[#D43953] bg-[#FFF5F6] px-4 outline-none'
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        type='submit'
+                        className='h-12 cursor-pointer rounded-lg bg-[#D43953] text-xl font-semibold text-white'
+                    >
+                        Cadastrar
+                    </button>
+                    <p className='text-center text-lg text-[#d43953]'>
+                        Já tem uma conta?{' '}
+                        <a
+                            href='login'
+                            className='font-medium'
+                        >
+                            Login
+                        </a>
+                    </p>
+                </form>
+            </main>
+        </div>
     );
 }
