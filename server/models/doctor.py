0abf9d9b84a5524f2ab23ca96893abe70a074ad7
Davi Francisco <franciscodavi327@gmail.com>
2026-08-22 12:51:34 -0300

    feat: adicionar funcionalidade de ativar ou inativar médico ao admin

--- server/models/doctor.py
+++ server/models/doctor.py
@@ -1,6 +1,15 @@
-from sqlalchemy import ForeignKey, String
+import enum
+
+from sqlalchemy import Enum, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
+
 from models.user import User, UserType
+
+
+class Status(enum.Enum):
+    PENDING = 'pending'
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
 
 
 class Doctor(User):
@@ -9,5 +18,6 @@ class Doctor(User):
     id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True, nullable=False)
     specialty: Mapped[str] = mapped_column(String(100), nullable=False)
     crm: Mapped[str] = mapped_column(String(50), index=True, nullable=False)
+    status: Mapped[Status] = mapped_column(Enum(Status), default=Status.PENDING, nullable=False)
 
     __mapper_args__ = {'polymorphic_identity': UserType.DOCTOR}
--- server/routes/admin.py
+++ server/routes/admin.py
@@ -1,70 +1,57 @@
-from fastapi import APIRouter, Depends, HTTPException, Response
-from pwdlib import PasswordHash
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError, OperationalError
-from database import get_session
-from models.doctor import Doctor
-from schemas.doctor import DoctorCreate
 from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError, OperationalError
+from sqlalchemy.orm import Session
+
+from database import get_session
+from models.doctor import Doctor, Status
+from models.user import User, UserType
+from routes.user import get_current_user
+from schemas.doctor import DoctorCreate
 
 
 router = APIRouter(prefix='/admin', tags=['admin'])
 SessionDep = Annotated[Session, Depends(get_session)]
-ph = PasswordHash.recommended()
 
 
-@router.post('/doctor')
-def create_doctor(session: SessionDep, user_input: DoctorCreate, response: Response):
-    try:
-        doctor = Doctor(
-            name=user_input.name,
-            email=user_input.email,
-            password=ph.hash(user_input.password),
-            specialty=user_input.specialty,
-            crm=user_input.crm
-        )
-        session.add(doctor)
-        session.commit()
-
-        return {'ok': True, 'message': 'Médico adicionado com sucesso'}
-
-    except IntegrityError:
-        session.rollback()
-        raise HTTPException(status_code=409, detail='Credenciais inválidas')
-
-
-@router.put('/doctor/{doctor_id}')
-def update_doctor(session: SessionDep, user_input: DoctorCreate, doctor_id: int):
-    doctor = session.get(Doctor, doctor_id)
-    if doctor is None:
-        raise HTTPException(status_code=404, detail='Médico não encontrado')
+@router.patch('/doctors/{doctor_id}/active', response_model=DoctorCreate)
+def approve_doctor(session: SessionDep, doctor_id: int, user: Annotated[User, Depends(get_current_user)]):
+    if not user or user.type != UserType.ADMIN:
+        raise HTTPException(status_code=403, detail='Acesso negado')
 
     try:
-        doctor.name = user_input.name
-        doctor.email = user_input.email
-        doctor.password = ph.hash(user_input.password)
-        doctor.specialty = user_input.specialty
-        doctor.crm = user_input.crm
+        doctor = session.get(Doctor, doctor_id)
+        if not doctor:
+            raise HTTPException(status_code=404, detail='Médico não encontrado')
+
+        doctor.status = Status.ACTIVE
         session.commit()
-
-        return {'ok': True, 'message': 'Médico atualizado com sucesso'}
-
-    except IntegrityError:
-        session.rollback()
-        raise HTTPException(status_code=409, detail='Credenciais inválidas')
-
-
-@router.delete('/doctor/{doctor_id}')
-def delete_doctor(session: SessionDep, doctor_id: int):
-    doctor = session.get(Doctor, doctor_id)
-    if doctor is None:
-        raise HTTPException(status_code=404, detail='Médico não encontrado')
-
-    try:
-        session.delete(doctor)
-        session.commit()
-        return {'ok': True, 'message': 'Médico apagado com sucesso'}
+        return doctor
 
     except OperationalError:
-        session.rollback()
-        raise HTTPException(status_code=500, detail='Ocorreu um erro apagar o médico')
+        raise HTTPException(status_code=500, detail='Erro na conexão com o banco de dados')
+
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail='Erro de integridade ocorreu')
+
+
+@router.patch('/doctors/{doctor_id}/inactive', response_model=DoctorCreate)
+def deny_doctor(session: SessionDep, doctor_id: int, user: Annotated[User, Depends(get_current_user)]):
+    if not user or user.type != UserType.ADMIN:
+        raise HTTPException(status_code=403, detail='Acesso negado')
+
+    try:
+        doctor = session.get(Doctor, doctor_id)
+        if not doctor:
+            raise HTTPException(status_code=404, detail='Médico não encontrado')
+
+        doctor.status = Status.INACTIVE
+        session.commit()
+        return doctor
+
+    except OperationalError:
+        raise HTTPException(status_code=500, detail='Erro na conexão com o banco de dados')
+
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail='Erro de integridade ocorreu')
